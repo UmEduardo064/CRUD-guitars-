93de8c7644831fb381bc10a92808b08c57107e04
Eduardo Sales <eduardosalespf064@gmail.com>
2025-08-26 04:11:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <html lang="pt">
 <head>
         <meta charset="utf-8">
@@ -10,7 +9,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-                <a class="navbar-brand" href="index.html">Gitars🤘</a>
+                <a class="navbar-brand" href="index.html">Guitars🤘</a>
 
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
